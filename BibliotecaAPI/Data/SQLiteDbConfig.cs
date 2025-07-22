fc25671571d498b28167dc022b34bf9b8ac2ae7b
Moises_Xavier <secondmox91@gmail.com>
2025-07-22 20:27:55 -0300

    Add DefaultConnection in appsetings and Scope in Program class

--- BibliotecaAPI/Data/SQLiteDbConfig.cs
+++ BibliotecaAPI/Data/SQLiteDbConfig.cs
@@ -1,143 +1,162 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.Data.Sqlite;
 
 namespace BibliotecaAPI.Data
 {
-    public static class SQLiteDbConfig
+    public class SQLiteDbConfig
     {
+        private readonly IConfiguration _configuration;
         private static string _connectionString;
-        private static readonly string DbFileName = "Biblioteca.db";
+
+        public SQLiteDbConfig(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            string rawConnectionString = _configuration.GetConnectionString("DefaultConnection");
+
+            if (string.IsNullOrEmpty(rawConnectionString))
+            {
+                throw new InvalidOperationException("A string de conexão 'DefaultConnection' não foi encontrada em appsettings.json.");
+            }
+
+            if (!rawConnectionString.Contains("Data Source="))
+            {
+                string dbFileName = rawConnectionString;
+                string dbPath = Path.Combine(AppContext.BaseDirectory, dbFileName);
+                _connectionString = $"Data Source={dbPath}";
+            }
+            else
+            {
+                _connectionString = rawConnectionString;
+            }
+        }
 
         public static string GetConnectionString()
         {
-            if (string.IsNullOrEmpty(_connectionString))
-            {
-                string dbPath = Path.Combine(AppContext.BaseDirectory, DbFileName);
-                _connectionString = $"Data Source={dbPath}";
-            }
             return _connectionString;
         }
 
-        public static void InitializeDatabase()
+        public void InitializeDatabase()
         {
             using (var connection = new SqliteConnection(GetConnectionString()))
             {
                 connection.Open();
                 var command = connection.CreateCommand();
 
+                using (var pragmaCommand = new SqliteCommand("PRAGMA foreign_keys = ON;", connection))
+                {
+                    pragmaCommand.ExecuteNonQuery();
+                }
+
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Client (
-                        id char(26) [primary key]
-                        created_at timestamp [default: now()]
-                        updated_at timestamp [default: now()]
+                        id char(26) PRIMARY KEY,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                        name varchar [not null]
-                        email varchar [not null]
+                        name varchar NOT NULL,
+                        email varchar NOT NULL,
                         phone char(14)
-                    )
+                    );
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Catalog (
-                    id char(13) [primary key] // ISBN
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()] 
+                        id char(13) PRIMARY KEY,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
 
-                    title varchar [not null]
-                    author varchar [not null]
-                    year int [not null]
-                    rev int [not null]
-                    publisher_id int [not null]
-                    pages int [not null]
-                    synopsis varchar 
-                    language_id int [not null]
-                    is_foreign bool [not null, default: false]
-                    )
+                        title varchar NOT NULL,
+                        author varchar NOT NULL,
+                        year INTEGER NOT NULL,
+                        rev INTEGER NOT NULL,
+                        pages INTEGER NOT NULL,
+                        synopsis varchar ,
+                        is_foreign INTEGER NOT NULL DEFAULT 0,
+                        publisher_id INTEGER,
+                        language_id INTEGER,
+                        FOREIGN KEY (publisher_id) REFERENCES Publisher(id),
+                        FOREIGN KEY (language_id) REFERENCES Language(id)
+                    );
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Genrer (
-                    id int [primary key, increment]
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()] 
-                    
-                    value varchar
-                    )
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+                        
+                        value varchar NOT NULL
+                    );
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS CatalogGenre (
-                    catalog_id char(13)
-                    genre_id int
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()]
-                    )
-                    Ref: Catalog.id > CatalogGenre.catalog_id
-                    Ref: Genrer.id > CatalogGenre.genre_id
-
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        catalog_id char(13),
+                        genrer_id INTEGER,
+                        FOREIGN KEY (catalog_id) REFERENCES Catalog(id),
+                        FOREIGN KEY (genrer_id) REFERENCES Genrer(id)
+                    );
+                    
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Publisher (
-                    id int [primary key, increment]
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()]
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                    name varchar [not null]   
-                    )
-                    Ref: Publisher.id - Catalog.publisher_id
-
+                        name varchar NOT NULL
+                    );
+                    
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Language (
-                    id int [primary key, increment]
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()] 
+                        id INTEGER PRIMARY KEY AUTOINCREMENT,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
 
-                    name varchar [not null]  
-                    )
-                    Ref: Language.id - Catalog.language_id
-
+                        name varchar NOT NULL
+                    );
+                    
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Inventory (
-                    id char(26) [primary key]
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()]
+                        id char(26) PRIMARY KEY,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                    catalog_id char(13)  
-                    condition int [not null, default: 100]
-                    is_available bool [not null, default: true]
-                    )
-                    Ref: Inventory.catalog_id > Catalog.id
-
+                        condition INTEGER NOT NULL DEFAULT 100,
+                        is_available INTEGER NOT NULL DEFAULT 1,
+                        catalog_id char(13),
+                        FOREIGN KEY (catalog_id) REFERENCES Catalog(id)
+                    );
+                    
                 ";
                 command.ExecuteNonQuery();
 
                 command.CommandText = @"
                     CREATE TABLE IF NOT EXISTS Loan (
-                    id char(26) [primary key]
-                    created_at timestamp [default: now()]
-                    updated_at timestamp [default: now()]
+                        id char(26) PRIMARY KEY,
+                        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 
-                    client_id char(26) [not null]
-                    inventory_id char(26) [not null]
-                    days_to_expire int [not null, default: 30]
-                    returned_at timestamp [default: null]
-                    )
-                    Ref: Client.id < Loan.client_id
-                    Ref: Inventory.id < Loan.inventory_id
+                        days_to_expire INTEGER NOT NULL DEFAULT 30,
+                        returned_at TIMESTAMP DEFAULT NULL,
+                        client_id char(26),
+                        inventory_id char(26),
+                        FOREIGN KEY (client_id) REFERENCES Client(id),
+                        FOREIGN KEY (inventory_id) REFERENCES Inventory(id)
+                    );
+                    
                 ";
                 command.ExecuteNonQuery();
             }
--- BibliotecaAPI/Program.cs
+++ BibliotecaAPI/Program.cs
@@ -1,10 +1,14 @@
+using BibliotecaAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddSingleton<SQLiteDbConfig>();
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
 
 var app = builder.Build();
 
@@ -12,6 +16,21 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    try
+    {
+        var sqliteDbConfig = services.GetRequiredService<SQLiteDbConfig>();
+        sqliteDbConfig.InitializeDatabase();
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"Erro crítico ao inicializar o banco de dados: {ex.Message}");
+        throw;
+    }
 }
 
 app.UseHttpsRedirection();

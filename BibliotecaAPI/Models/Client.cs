811b5cb167ecde46ac717a19457c4962e9a02866
Moises_Xavier <secondmox91@gmail.com>
2025-07-22 22:09:09 -0300

    Add Constructors

--- BibliotecaAPI/Models/Client.cs
+++ BibliotecaAPI/Models/Client.cs
@@ -3,13 +3,30 @@ using System.ComponentModel.DataAnnotations;
 namespace BibliotecaAPI.Models
 {
     public class Client
-    {   
+    {
         [Key]
-        public string Id { get; set; } = Ulid.NewUlid().ToString();
+        public string Id { get; set; }
+        public TimeSpan CreatedAt { get; set; }
+        public TimeSpan UpdatedAt { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime UpdatedAt { get; set; }
+
+        public Client()
+        {
+            Id = Ulid.NewUlid().ToString();
+            CreatedAt = DateTime.Now.TimeOfDay;
+            UpdatedAt = DateTime.Now.TimeOfDay;
+        }
+
+        public Client(string id, TimeSpan createdAt, TimeSpan updatedAt, string name, string email, string phone)
+        {
+            Id = id;
+            CreatedAt = createdAt;
+            UpdatedAt = updatedAt;
+            Name = name;
+            Email = email;
+            Phone = phone;
+        }
     }
 }

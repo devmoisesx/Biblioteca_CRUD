44399e63281d59074aec16f934bcc23920c0ed9b
Moises_Xavier <secondmox91@gmail.com>
2025-07-22 21:39:03 -0300

    Create ServiceClient class

---
+++ BibliotecaAPI/Services/ServiceClient.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BibliotecaAPI.Interface;
+using BibliotecaAPI.Models;
+using BibliotecaAPI.Storages;
+
+namespace BibliotecaAPI.Services
+{
+    public class ServiceClient : IServiceClient
+    {
+        private readonly StorageClient _storage;
+
+        public ServiceClient(StorageClient storage)
+        {
+            _storage = storage;
+        }
+    }
+}

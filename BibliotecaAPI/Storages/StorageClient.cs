d3b86778d88bf741e87b74657b08af8c5cedd519
Moises_Xavier <secondmox91@gmail.com>
2025-07-22 21:37:46 -0300

    Create StorageClient with Add method

--- BibliotecaAPI/Storages/StorageClient.cs
+++ BibliotecaAPI/Storages/StorageClient.cs
@@ -1,3 +1,7 @@
+using BibliotecaAPI.Data;
+using BibliotecaAPI.Models;
+using Microsoft.Data.Sqlite;
+
 namespace BibliotecaAPI.Storages
 {
     public class StorageClient
@@ -9,6 +13,47 @@ namespace BibliotecaAPI.Storages
         {
             _connectionString = _configuration.GetConnectionString("DefaultConnection");
         }
+
+        public async void AddClient(Client client)
+        {
+            await using (var connection = new SqliteConnection(_connectionString))
+            {
+                connection.Open();
+                var command = connection.CreateCommand();
+
+                command.CommandText = @"
+                    INSERT INTO Transacoes (updated_at, name, email, phone) 
+                    VALUES (@UpdatedAt, @Name, @Email, @Phone);
+                ";
+
+                command.Parameters.AddWithValue("@UpdatedAt", client.UpdatedAt);
+                command.Parameters.AddWithValue("@Name", client.Name);
+                command.Parameters.AddWithValue("@Email", client.Email);
+                command.Parameters.AddWithValue("@Phone", client.Phone);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        // public Client GetClientById(string id)
+        // {
+        //     return Client
+        // }
+
+        // public List<Client> GetClients()
+        // {
+            
+        // }
+
+        // public void UpdateClient(Client client)
+        // {
+            
+        // }
+
+        // public void DeleteClient(Client client)
+        // {
+            
+        // }
 
     }
 }

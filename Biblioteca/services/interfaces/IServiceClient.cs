f34da59087c19bba1585c3ab4097a85527f9e16b
Moises_Xavier <secondmox91@gmail.com>
2025-07-25 19:11:28 -0300

    Remove unnecessary files

--- Biblioteca/services/interfaces/IServiceClient.cs
+++
@@ -1,8 +0,0 @@
-public interface IServiceClient
-{
-    void RegisterClient(Client client);
-    Client GetClient(string id);
-    List<Client> GetClients();
-    Client UpdateClient(Client client);
-    Client DeleteClient(string id);
-}
